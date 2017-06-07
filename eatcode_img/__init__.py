c4530fbbf751d73f30c012effcc5c7e0602ecf67
Ayub Malik <ayub.malik@gmail.com>
2017-06-07 20:47:43 +0100

    update Image

--- eatcode_img/__init__.py
+++ eatcode_img/__init__.py
@@ -6,6 +6,7 @@ class Image:
         self.height = height
         self.width = width
         self.format = format
+        self.orientation = "portrait" if self.height > self.width else "landscape"
 
     def __str__(self):
-        return "Image({}, {}x{}, {}).".format(self.name, self.height, self.width, self.format)
+        return "Image({}, {}x{}, {}, {}).".format(self.name, self.height, self.width, self.format, self.orientation)
