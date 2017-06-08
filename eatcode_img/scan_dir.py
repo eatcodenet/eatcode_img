d1380dfe4bd4649152e5900f5b197a0e6034b483
Ayub Malik <ayub.malik@gmail.com>
2017-06-08 16:36:06 +0100

    wip

--- eatcode_img/scan_dir.py
+++ eatcode_img/scan_dir.py
@@ -8,7 +8,9 @@ def scan(img_dir):
     _images = []
     for entry in os.scandir(img_dir):
         cmd = ["identify", "-format", "%h,%w,%b,%m,%M", (img_dir + os.sep + entry.name)]
-        output = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode("UTF-8").strip()
+        process = subprocess.run(cmd, stdout=subprocess.PIPE)
+        print(process)
+        output = process.stdout.decode("UTF-8").strip()
         print("*** {} ***".format(output))
         (_height, _width, _size_bytes, _image_format, _name) = output.split(",")
         _images.append(eatcode_img.Image(_name, int(_height), int(_width), int(_size_bytes[:-1]), _image_format))
