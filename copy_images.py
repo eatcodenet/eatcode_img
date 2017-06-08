c2a7843c5af3e28e3b4a5a012929ff5fd4202edc
Ayub Malik <ayub.malik@gmail.com>
2017-06-08 16:45:07 +0100

    wip

--- copy_images.py
+++ copy_images.py
@@ -20,7 +20,7 @@ def _init():
 def copy_images(src_dir, dest_dir, filter_predicate):
     images = eatcode_img.scan_dir.scan(src_dir)
     print("i." + filter_predicate)
-    filtered = [i for i in images if not eval("i." + filter_predicate)]
+    filtered = [i for i in images if eval("i." + filter_predicate)]
     for src_img in filtered:
         logging.debug("copying image {} to dir {}".format(src_img, dest_dir))
         shutil.copy2(src_img.name, dest_dir)
